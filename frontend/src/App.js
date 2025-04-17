ec253a29c625dbc40f15aa390413c73816b8f1c1
Tika299 <vukofa9120@gmail.com>
2025-04-17 14:46:25 +0700

    commit Router Link slider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Home';
+import Transaction from './Transaction';
 
 function App() {
   return (
-    <div>
-      <Home/> 
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transactions" element={<Transaction />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/Slider.js
+++ frontend/src/Slider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Slider() {
     return (
@@ -18,20 +19,20 @@ function Slider() {
             <hr />
             <ul className="nav nav-pills flex-column mb-auto">
                 <li className="nav-item">
-                    <a href="#" className="nav-link active" aria-current="page">
+                    <Link to="/" className="nav-link active" aria-current="page">
                         <svg className="bi me-2" width={16} height={16}>
                             <use xlinkHref="#home" />
                         </svg>
                         Home
-                    </a>
+                    </Link>
                 </li>
                 <li>
-                    <a href="#" className="nav-link text-white">
+                    <Link to="/transactions" className="nav-link text-white">
                         <svg className="bi me-2" width={16} height={16}>
                             <use xlinkHref="#speedometer2" />
                         </svg>
-                        Dashboard
-                    </a>
+                        Transaction
+                    </Link>
                 </li>
                 <li>
                     <a href="#" className="nav-link text-white">
